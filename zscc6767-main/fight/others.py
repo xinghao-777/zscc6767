9c42b60e00a9d7c259ef92e1e550e2a2893e23dd
unp0wer <154443178+unp0wer@users.noreply.github.com>
2024-06-01 13:56:31 +0800

    Delete zscc6767-main/fight directory

--- zscc6767-main/fight/others.py
+++
@@ -1,5 +0,0 @@
-def calculate_hex_rc(row, col):  # 用于计算给定行和列索引的六边形的以中心点为（0，0）分布的坐标。
-    r = row - (13 + 1) // 2
-    c = ((col - 1 / 2) - ((13 - 1) // 2 + row) / 2) * 2
-    return r, c
-print(calculate_hex_rc(7,7))
